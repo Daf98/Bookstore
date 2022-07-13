daf44921e2a1a09ae2b3af649f36a6e2ebe0adc0
Daf <dafne.azzolina@gmail.com>
2022-07-12 22:45:01 -0300

    Link React and Redux

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Categories from './components/Categories';
+import ReduxBooks from './ReduxBooks';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <ReduxBooks />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/categories" element={<Categories />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
