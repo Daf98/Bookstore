ccc17a3c4fbf552f196010d3498a1c459c59338c
Daf <dafne.azzolina@gmail.com>
2022-07-14 15:49:08 -0300

    Add percentage circle

--- src/components/ReduxBooks.js
+++ src/components/ReduxBooks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeBookFromAPI } from '../redux/API/api';
 import { removeBook } from '../redux/books/books';
+import Percentage from './Percentage';
 import '../styles/ReduxBooks.css';
 
 const ReduxBooks = () => {
@@ -12,27 +13,30 @@ const ReduxBooks = () => {
       {books ? (
         books.map((book) => (
           <div className="book-container" key={book.id}>
-            <div className="book-info">
-              <p className="category">Fiction</p>
-              <h1>{book.title}</h1>
-              <h4 className="author">{book.author}</h4>
+            <div className="info-container">
+              <div className="book-info">
+                <p className="category">Fiction</p>
+                <h1>{book.title}</h1>
+                <h4 className="author">{book.author}</h4>
+              </div>
+              <div className="button-container">
+                <button type="button" className="book-buttons">Comments</button>
+                <div><p className="button-divider">|</p></div>
+                <button
+                  className="book-buttons"
+                  type="button"
+                  onClick={() => {
+                    removeBookFromAPI(book.id);
+                    dispatch(removeBook(book.id));
+                  }}
+                >
+                  Remove
+                </button>
+                <div><p className="button-divider">|</p></div>
+                <button type="button" className="book-buttons">Edit</button>
+              </div>
             </div>
-            <div className="button-container">
-              <button type="button" className="book-buttons">Comments</button>
-              <div><p>|</p></div>
-              <button
-                className="book-buttons"
-                type="button"
-                onClick={() => {
-                  removeBookFromAPI(book.id);
-                  dispatch(removeBook(book.id));
-                }}
-              >
-                Remove
-              </button>
-              <div><p>|</p></div>
-              <button type="button" className="book-buttons">Edit</button>
-            </div>
+            <div className="map-container"><Percentage /></div>
           </div>
         ))
       ) : (
